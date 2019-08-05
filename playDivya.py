58fff8da58ce3d58af1aa19901a5e1fc53f2a359
Mustafa Hoda <mustafahoda@mustafas-mbp.appfiniti.com>
2019-08-05 17:23:12 -0500

    No longer hard-coding the time to wait. Actually getting the lenght of video and sleeping that long

--- playDivya.py
+++ playDivya.py
@@ -1,19 +1,60 @@
 import time
+
+from moviepy.editor import VideoFileClip
+
+
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('https://www.instagram.com/divyamsodhi/')
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
-firstBox = browser.find_element_by_class_name('_bz0w')
-firstBox.click()
 
-time.sleep(2)
-#
-for x in range(0,10):
-    time.sleep(2)
-    secondBox = browser.find_element_by_class_name('OAXCp')
-    secondBox.click()
+def open_page():
+    browser = webdriver.Firefox()
+    browser.get('https://www.instagram.com/divyamsodhi/')
+    return browser
 
-    time.sleep(10)
+def click_on_first_video(browser):
+    firstThumbnail = WebDriverWait(browser, 2).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "_bz0w"))
+    )
+    firstThumbnail.click()
 
-    next = browser.find_element_by_class_name('coreSpriteRightPaginationArrow')
-    next.click()
+def getVideoURL():
+    time.sleep(0.5)
+    videoTag = browser.find_element_by_css_selector('video')
+    videoURL = videoTag.get_attribute("src")
+    return videoURL
+
+def getVideoDuration(videoURL):
+    clip = VideoFileClip(videoURL)
+    return clip.duration
+
+
+def loop_through_videos():
+    for i in range(10):
+
+        videoURL = getVideoURL()
+        videoDuration = getVideoDuration(videoURL)
+
+        print("videoURL: %s" % videoURL)
+        print("videoDuration: %s" % videoDuration)
+
+        playBtn = WebDriverWait(browser, 2).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "OAXCp"))
+        )
+        playBtn.click()
+
+        time.sleep(videoDuration)
+
+        nextBtn = WebDriverWait(browser, 2).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "coreSpriteRightPaginationArrow"))
+        )
+        nextBtn.click()
+
+
+if __name__ == "__main__":
+    browser = open_page()
+    click_on_first_video(browser=browser)
+    loop_through_videos()
